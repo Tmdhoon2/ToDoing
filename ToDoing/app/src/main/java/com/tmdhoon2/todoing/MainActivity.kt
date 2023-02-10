71793ac2dc19268420609cdb96767da1754016c7
Tmdhoon2 <hks026naver@gmail.com>
2023-02-11 00:55:44 +0900

    ⚙️ :: initialize project

---
+++ ToDoing/app/src/main/java/com/tmdhoon2/todoing/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.tmdhoon2.todoing
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
